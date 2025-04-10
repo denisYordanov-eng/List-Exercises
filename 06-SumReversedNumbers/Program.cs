0712973b536ca91b9b80fb2c5921a0be6cb99718
denisYordanov-eng <denisivov437@gmail.com>
2025-04-10 05:24:03 +0300

    Just made a task.

---
+++ 06-SumReversedNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            var revesedString = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                string str = input[i];
+                string reveresedInput = new string(str.Reverse().ToArray());
+                revesedString.Add(reveresedInput);
+            }
+            var reversedNumbers = new List<int>();
+            for (int i = 0; i < revesedString.Count; i++)
+            {
+                reversedNumbers.Add(int.Parse(revesedString[i]));
+            }
+            int sum = reversedNumbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
