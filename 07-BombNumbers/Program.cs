35ea1e0a80416c489152199522672f51e98a20d8
denisYordanov-eng <denisivov437@gmail.com>
2025-04-10 06:15:40 +0300

    Just made a task.

---
+++ 07-BombNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int[] commands = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int bomb = commands[0];
+            int power = commands[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb)
+                {
+                    int index = i;
+                    int leftBombDemage = Math.Max(index  - power, 0);
+                    int rightBombDemage = Math.Min(index + power, numbers.Count - 1);
+
+                    int removeNumbers = rightBombDemage - leftBombDemage + 1;
+                    numbers.RemoveRange(leftBombDemage, removeNumbers);
+
+                    i = -1;
+                }
+            }
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
